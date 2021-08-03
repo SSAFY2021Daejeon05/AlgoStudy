052173bf45c7544fa3f80c63d41ad1b9045ef411
suyeonkkk <tnehd158@gmail.com>
2021-08-03 21:02:34 +0900

    solution_1

---
+++ Week01/Suyeonkkk/src/problem_1/Solution_1.java
@@ -0,0 +1,13 @@
+package problem_1;
+
+class Solution_1 {
+    public long solution(int price, int money, int count) {
+        long totalPrice = 0;
+        for (int i = 1; i <= count; i++)
+            totalPrice += (long) (price * i);
+        
+        if (totalPrice - (long) money > 0)
+            return totalPrice - (long) money;
+        return 0;
+    }
+}
