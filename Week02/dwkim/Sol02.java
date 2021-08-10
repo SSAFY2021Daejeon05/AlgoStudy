0ad831532fbbb9b1d60fc35ccd25059947a1430d
web-doh <66818228+web-doh@users.noreply.github.com>
2021-08-10 19:23:56 +0900

    [week02] Submit solution 02: 로또의 최고순위와 최저순위

--- Week02/dwkim/Sol02.java
+++ Week02/dwkim/Sol02.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {7, 7};
+        int min = 7;
+        int max = 7;
         
         for(int n: lottos){
             if(n == 0){
-                answer[0] -= 1;
+                min -= 1;
                 continue;
             }
             for(int i = 0; i < 6; i++){
                 if(win_nums[i] == n) {
-                    answer[1] -= 1;
+                    max -= 1;
                     break;
                 }
             }
         }
         
-        answer[0] -= (7 - answer[1]);
+        min -= (7 - max);
         
-        if(answer[1] > 6) answer[1] = 6;
-        if(answer[0] > 6) answer[0] = 6;
+        if(max > 6) max = 6;
+        if(min > 6) min = 6;
         
-        return answer;
+        return new int[]{min, max};
     }
 }
