e6464df666bcba628999e0d22ecaa929cf5d5a7c
suyeonkkk <tnehd158@gmail.com>
2021-08-18 19:41:05 +0900

    solution_2 음양 더하기

---
+++ Week03/Suyeonkkk/src/Solution_2.java
@@ -0,0 +1,13 @@
+// 음양 더하기
+class Solution_2 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        int length = absolutes.length;
+        
+        for (int i = 0; i < length; ++i) {
+            if (signs[i]) answer += absolutes[i];
+            else answer -= absolutes[i];
+        }
+        return answer;
+    }
+}
