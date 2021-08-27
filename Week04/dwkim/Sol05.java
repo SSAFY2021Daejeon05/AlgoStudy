7ddb6ddb6d1939c16960516cf5a1726cce25eebe
web-doh <66818228+web-doh@users.noreply.github.com>
2021-08-27 22:51:53 +0900

    [week04] Submit solution 05: 3진법 뒤집기

--- Week04/dwkim/Sol05.java
+++ Week04/dwkim/Sol05.java
@@ -1,19 +1,9 @@
 class Solution {
-    public int solution(int n) {
-	int answer = 0;
-		
-		String reversed = toReversedTernary(n);
-		int len = reversed.length() - 1;
-		
-        // 10진수로 만들기
-		for(int i = len; i >= 0; i--) {
-			answer += Math.pow(3, len - i) * (reversed.charAt(i) - '0');
-		}
-		
-		return answer;
-	}
+   	public int solution(int n) {
+		return Integer.parseInt(toReversedTernary(n), 3);
+    	}
 	
-    // 거꾸로된 3진수 만들기
+    	// 거꾸로된 3진수 만들기
 	static String toReversedTernary(int n) {
 		StringBuilder t = new StringBuilder();
 		
