56459e9968c2727a9fd4e60c886f3ea7403da4b9
Kyle Kim <dkim577@wisc.edu>
2021-09-27 19:43:08 +0900

    re

--- Week07/DEHO/입실퇴실.java
+++ Week07/DEHO/입실퇴실.java
@@ -1,18 +1,20 @@
-def solution(enter, leave):
-    answer = [0] * len(enter)
+class Solution { .
+    public int[] solution(int[] enter, int[] leave) {
+        int len = enter.length;
+        int[] answer = new int[len], idx = new int[len + 1];
+        for(int i = 0; i < len; i++) idx[enter[i]] = i;
+        for(int i = 0; i < len; i++) {
+            for(int j = 0, late = idx[enter[i]]; j < len; j++) {
+                int e = enter[i], l = leave[j];
+                if(e == l) break;
+                if(idx[l] > late || (late > idx[e] && idx[l] < late)) {
+                    answer[l-1]++;
+                    answer[e-1]++;
+                    late = Math.max(idx[l], late);
+                }
+            }
+        }
+        return answer;
+    }
+}
 
-    room = []
-    idx = 0
-    for l in leave:
-        print(l)
-        while l not in room:
-            room.append(enter[idx])
-            idx += 1
-        print(room)
-        room.remove(l)
-        print(room)
-        for person in room:
-            answer[person - 1] += 1
-        answer[l - 1] += len(room)
-
-    return answer
